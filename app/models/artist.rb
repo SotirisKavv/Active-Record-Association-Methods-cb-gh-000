740f063703437b58ca618d5ba7f36e3987887612
Sotiris Kavvouras <sotos.kavvouras@gmail.com>
2019-09-21 12:09:56 +0000

    Automatically backed up by Learn

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -3,14 +3,14 @@ class Artist < ActiveRecord::Base
   has_many :genres, through: :songs
 
   def get_genre_of_first_song
-    Artist.songs[0].genre
+    self.songs[0].genre
   end
 
   def song_count
-    Artist.songs.size
+    self.songs.size
   end
 
   def genre_count
-    Artist.genres.size
+    self.genres.size
   end
 end
